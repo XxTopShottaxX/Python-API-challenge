f62081e4785a543465b37cb0ec87e9718eba0c75
Jerome <xxtopshottaxx@gmail.com>
2020-01-29 00:31:29 -0500

    HW update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "58d8fb8ea04b85df327bf2aea1d25a06"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB5pzvcVCQEbCK2Xzid3IXFPRYy6tD7BJc"
